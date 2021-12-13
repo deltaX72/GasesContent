641ccf11acb644393ccbb8c989d80a37f3bba706
rudi <rudolf.saribekyan@mail.ru>
2021-12-14 02:09:27 +0700

    finish languages

--- gases-content-ui/src/i18next.js
+++ gases-content-ui/src/i18next.js
@@ -8,7 +8,7 @@ i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        supportedLngs: ['ru', 'en'],
+        supportedLngs: ['ru', 'en','am'],
         fallbackLng: 'en',
         debug: false,
         detection: {
