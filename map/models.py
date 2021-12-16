ecced9ea1fbf15b3db0844b4b161b36266deb656
Ilya <50233011+deltaX72@users.noreply.github.com>
2021-12-16 14:43:23 +0700

    config data handle and calculations with satellites data (#10)

--- map/models.py
+++ map/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-from gasescontent.settings import \
+from gasescontent.constants import \
     MAX_GAS_NAME_LENGTH, \
-    MAX_GOSAT_DATA_VERSION_FIELD_LENGTH
+    MAX_GOSAT_DATA_VERSION_FIELD_LENGTH, \
+    MAX_SATELLITE_NAME_LENGTH
 
 
 class UserInputData(models.Model):
@@ -14,30 +15,107 @@ class UserInputData(models.Model):
     date_to = models.DateField()
 
 
-class BaseSatelliteData(models.Model):
+class Satellite(models.Model):
+    satellite_name = models.CharField(max_length=MAX_SATELLITE_NAME_LENGTH, verbose_name='satellite_name')
+    gas_name = models.CharField(max_length=MAX_GAS_NAME_LENGTH, verbose_name='gas_name')
+
+    @classmethod
+    def create(cls, satellite_name: str, gas_name: str):
+        return cls(satellite_name, gas_name)
+
+    class Meta:
+        managed = True
+        db_table = 'satellite_data'
+
+
+class Accuracy(models.Model):
+    satellite_id = models.ForeignKey("Satellite", on_delete=models.CASCADE, verbose_name='ID')
+    accuracy_value = models.CharField(max_length=32)
+    pixel_size = models.IntegerField()
+
+    @classmethod
+    def create(cls, accuracy_value, pixel_size):
+        return cls(accuracy_value, pixel_size)
+
+    class Meta:
+        managed = True
+        db_table = 'accuracy_data'
+
+
+class GOSATDataQuerySet(models.QuerySet):
+    def get_unique_by_time(self, time):
+        return self.filter(time=time)
+
+
+class GOSATDataManager(models.Manager):
+    def get_queryset(self):
+        return GOSATDataQuerySet(self.model, using=self._db)
+
+    def get_rows_by_time_unique(self, time):
+        return self.get_queryset().get_unique_by_time(time)
+
+
+class OCO2DataQuerySet(models.QuerySet):
+    def get_unique_by_time(self, time):
+        return self.filter(time=time)
+
+
+class OCO2DataManager(models.Manager):
+    def get_queryset(self):
+        return OCO2DataQuerySet(self.model, using=self._db)
+
+    def get_rows_by_time_unique(self, time):
+        return self.get_queryset().get_unique_by_time(time)
+
+
+class GOSATData(models.Model):
     year = models.IntegerField()
     month = models.IntegerField()
     day = models.IntegerField()
-    time = models.TimeField()
+    hour = models.IntegerField()
+    minute = models.IntegerField()
+    second = models.IntegerField()
+    time = models.IntegerField(primary_key=True, unique=True)
     gas_name = models.CharField(max_length=MAX_GAS_NAME_LENGTH)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    content = models.FloatField()
-    temperature = models.FloatField()
-
-    class Meta:
-        unique_together = (('year', 'month', 'day', 'time'),)
-        db_table = 'base_satellite_data'
-        abstract = True
-
-
-class GOSATData(BaseSatelliteData):
+    gas_content = models.FloatField()
     data_version = models.CharField(max_length=MAX_GOSAT_DATA_VERSION_FIELD_LENGTH)
 
+    data = GOSATDataManager()
+
+    @classmethod
+    def create(cls, year, month, day, hour, minute, second, time, gas_name, latitude, longitude, gas_content, data_version):
+        return cls(
+            year, month, day, hour, minute, second, time, gas_name, latitude, longitude, gas_content, data_version
+        )
+
     class Meta:
+        managed = True
         db_table = 'gosat_data'
 
 
-class OCO2Data(BaseSatelliteData):
+class OCO2Data(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+    hour = models.IntegerField()
+    minute = models.IntegerField()
+    second = models.IntegerField()
+    time = models.IntegerField()
+    gas_name = models.CharField(max_length=MAX_GAS_NAME_LENGTH)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    gas_content = models.FloatField()
+
+    data = OCO2DataManager()
+
+    @classmethod
+    def create(cls, year, month, day, time, gas_name, latitude, longitude, gas_content):
+        return cls(
+            year, month, day, time, gas_name, latitude, longitude, gas_content
+        )
+
     class Meta:
+        managed = True
         db_table = 'oco2_data'
--- terminal/terminal.py
+++ terminal/terminal.py
@@ -1,19 +1,17 @@
 import re
+import tarfile
 from io import BytesIO
 
 import h5py
 import requests
+import pandas as pd
 
-from terminal.file_reader import read_co2_from_h5_file
-from terminal.utils import format_year_and_month
-from terminal.utils import read_archive, get_response
+from terminal.file_reader import read_co2_from_h5_file, read_co2_from_nc_file
+from terminal.utils import format_year_and_month, read_archive, check_if_not_200
 
 
 class GOSAT:
     url = 'https://data2.gosat.nies.go.jp/wgetdata/GU/SWIRL2CO2/'
-
-    GOSAT_USER = 'lit.ilya29@gmail.com'
-    GOSAT_PASSWORD = 'L57!EWCeSxh19cg_'
 
     def create_url(self, year: str, month: str, version: str):
         return f'{self.url}{year}/{self.get_filename_mask(year, month, version)}.tar'
@@ -22,25 +20,22 @@ class GOSAT:
     def get_filename_mask(year: str, month: str, version: str):
         return f'SWIRL2CO2_{year}{month}_{version}'
 
-    def download_archive(self, year: int, month: int, version: str):
+    def download_archive(self, session, year: int, month: int, version: str):
         y, m = format_year_and_month(year, month)
         url = self.create_url(y, m, version)
-        response = get_response(url=url, user=self.GOSAT_USER, password=self.GOSAT_PASSWORD)
 
-        if response.status_code != 200:
-            print(f'Can\'t find file (url: {url})')
-            return None
+        response = session.get(url=url, stream=True)
+        print(url)
 
-        return response.content
+        if check_if_not_200(response.status_code, url):
+            return None, None
+
+        return response.content, url
 
 
 class OCO2:
     url = 'https://oco2.gesdisc.eosdis.nasa.gov/data/OCO2_DATA/OCO2_L2_Lite_FP.9r/'
     oco2_gas_pattern = 'oco2_LtCO2_'
-
-    OCO2_USER = 'deltaX72'
-    OCO2_PASSWORD = 'kmAIK7M5apeLy02z'
-    REDIRECT_URI = 'https://oco2.gesdisc.eosdis.nasa.gov/data-redirect'
 
     def get_urls_list(self, year: int):
         response = requests.get(f'{self.url}{year}')
@@ -60,24 +55,59 @@ class OCO2:
         date = url[length:length + 6]
         return int('20' + date[:2]), int(date[2:4]), int(date[4:])
 
-    def download_file(self, url: str, year: int):
-        pass
+    def download_file(self, session, url: str, year: int):
+        file_url = f'{self.url}{year}/{url}'
+
+        response = session.get(url=file_url, stream=True)
+
+        if check_if_not_200(response.status_code, url):
+            return None, None
+
+        file = BytesIO()
+        for chunk in response.iter_content(chunk_size=128*1024):
+            if chunk:
+                file.write(chunk)
+
+        return file, url
 
 
 def gosat_run(year_from: int, year_to: int, month_from: int, month_to: int, version: str):
     gosat = GOSAT()
-    data = []
+    session = requests.session()
 
     for year in range(year_from, year_to + 1):
         for month in range(month_from, month_to + 1):
-            archive = gosat.download_archive(year, month, version)
+            archive, file_name = gosat.download_archive(session, year, month, version)
+            print(file_name)
 
             if archive is not None:
-                files = read_archive(BytesIO(archive))
+                files = read_archive(archive)
                 for i in files:
-                    h5_file = h5py.File(i, 'r')
-                    data.append(read_co2_from_h5_file(h5_file))
+                    read_co2_from_h5_file(h5py.File(i, 'r'), version)
             else:
                 print('Unknown file!')
 
-    return data
+
+def oco2_run(year_from: int, year_to: int, month_from: int, month_to: int, day_from: int, day_to: int):
+    oco2 = OCO2()
+    session = requests.session()
+
+    for year in range(year_from, year_to + 1):
+        urls = oco2.get_urls_list(year)
+
+        for url in urls:
+            y, m, d = oco2.get_url_date(url)
+            print(url)
+
+            if year_from <= y <= year_to and month_from <= m <= month_to and day_from <= d <= day_to:
+                file, file_name = oco2.download_file(session, url, y)
+                print(file_name)
+
+                if file is not None:
+                    nc4_file = h5py.File(file, 'r')
+                    read_co2_from_nc_file(nc4_file)
+                    nc4_file.close()
+                else:
+                    print(f'Can\'t find file (url: {url})')
+
+                file.close()
