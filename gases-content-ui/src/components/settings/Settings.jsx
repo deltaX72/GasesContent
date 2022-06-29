484a7448c1f2baa40f7852a8436ac7e60eb63095
Rudi <62327856+rudiSar@users.noreply.github.com>
2022-06-30 00:24:59 +0700

    creating_states (#14)
    * fixed adaptive and button close
    
    * created states for form

--- gases-content-ui/src/components/settings/Settings.jsx
+++ gases-content-ui/src/components/settings/Settings.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Settings.module.scss';
 import ButtonClose from "../UIelements/buttons/buttonClose/ButtonClose";
 import CustomButton from "../UIelements/buttons/customButton/CustomButton";
 import SimpleParagraph from "../UIelements/text/SimpleParagraph";
-import CustomInput from "../UIelements/input/CustomInput";
 import ButtonLeft from "../UIelements/buttons/buttonLeft/ButtonLeft";
 import ButtonRight from "../UIelements/buttons/buttonRight/ButtonRight";
-import CustomCheckBox from "../UIelements/checkbox/CustomCheckBox";
-import CustomRadio from "../UIelements/radio/CustomRadio";
 import {useTranslation} from "react-i18next";
 import HeaderText from "../UIelements/header/HeaderText";
 
@@ -15,7 +12,57 @@ const Settings = ({active, setActive, ...props}) => {
     let darkClass;
     props.darkmode ?  darkClass = classes.dark : darkClass = '';
 
+    const [minLat, setMinLat] = useState(56.44);
+    const [maxLat, setMaxLat] = useState(56.55);
+    const [minLong, setMinLong] = useState(84.9);
+    const [maxLong, setMaxLong] = useState(85);
+
+    const [dateFrom, setDateFrom] = useState("");
+    const [dateTo, setDateTo] = useState("");
+
+    const [gosat, setGosat] = useState(true);
+    const [gosat2, setGosat2] = useState(false);
+    const [oco2, setOco2] = useState(false);
+    const [oco3, setOco3] = useState(false);
+
+    const [co2, setCo2] = useState(true);
+    const [ch4, setCh4] = useState(!co2);
+
+    const [realData, setRealData] = useState(false);
+    const [middle, setMiddle] = useState(true);
+    const [high, setHigh] = useState(false);
+
     const { t } = useTranslation();
+
+    let data = {
+        minLat,
+        maxLat,
+        minLong,
+        maxLong,
+        dateFrom,
+        dateTo,
+        sat: {
+            gosat,
+            gosat2,
+            oco2,
+            oco3
+        },
+        gases: {
+            co2,
+            ch4
+        },
+        quality: {
+            high,
+            middle,
+            realData
+        }
+    }
+    console.log(data)
+
+    const changeGas = () => {
+        setCo2(!co2);
+        setCh4(!ch4);
+    }
 
     return (
         <div className={active ? `${classes.settings_common} ${classes.active} ${darkClass}` : `${classes.settings_common} ${darkClass}`} >
@@ -26,20 +73,52 @@ const Settings = ({active, setActive, ...props}) => {
             <div className={`${classes.flex_elem} ${darkClass}`}>
                 <SimpleParagraph children={t("latitude")} size='half' darkmode={props.darkmode} />
                 <SimpleParagraph children={t("longitude")} size='half' darkmode={props.darkmode} />
-
-                <CustomInput type='text' placeholder={t("min")} size='half' />
+                <input
+                    value={minLat}
+                    onChange={event => setMinLat(event.target.value)}
+                    placeholder={t("min")}
+                    type={props.type}
+                    className={`${classes.common_styles} ${classes.half}`}
+                />
                 -
-                <CustomInput type='text' placeholder={t("max")} size='half' />
-
-                <CustomInput type='text' placeholder={t("min")} size='half' />
+                <input
+                    value={maxLat}
+                    onChange={event => setMaxLat(event.target.value)}
+                    placeholder={t("max")}
+                    type={props.type}
+                    className={`${classes.common_styles} ${classes.half}`}
+                />
+                <input
+                    value={minLong}
+                    onChange={event => setMinLong(event.target.value)}
+                    placeholder={t("min")}
+                    type={props.type}
+                    className={`${classes.common_styles} ${classes.half}`}
+                />
                 -
-                <CustomInput type='text' placeholder={t("max")} size='half' />
+                <input
+                    value={maxLong}
+                    onChange={event => setMaxLong(event.target.value)}
+                    placeholder={t("max")}
+                    type={props.type}
+                    className={`${classes.common_styles} ${classes.half}`}
+                />
             </div>
             <div className={`${classes.flex_elem} ${darkClass}`}>
                 <SimpleParagraph children={t("date")} darkmode={props.darkmode} />
-                <CustomInput type='date' />
+                <input
+                    type="date"
+                    value={dateFrom}
+                    onChange={event => setDateFrom(event.target.value)}
+                    className={classes.common_styles}
+                />
                 -
-                <CustomInput type='date' />
+                <input
+                    type="date"
+                    value={dateTo}
+                    onChange={event => setDateTo(event.target.value)}
+                    className={classes.common_styles}
+                />
             </div>
 
             <CustomButton children={t("submit")} darkmode={props.darkmode} />
@@ -50,21 +129,122 @@ const Settings = ({active, setActive, ...props}) => {
             </div>
             <div className={`${classes.flex_elem} ${darkClass}`}>
                 <SimpleParagraph children={t("selectOfSatellites")} />
-                <CustomCheckBox darkmode={props.darkmode} checked={true} id='gosat' children='GOSAT' />
-                <CustomCheckBox darkmode={props.darkmode} id='gosat_2' children='GOSAT-2' />
-                <CustomCheckBox darkmode={props.darkmode} id='oco_2' children='OCO-2' />
-                <CustomCheckBox darkmode={props.darkmode} id='oco_3' children='OCO-3' />
+                <div className={`${classes.container_style} ${darkClass}`}>
+                    <input
+                        id={"gosat"}
+                        className={`${classes.checkbox_style}`}
+                        checked={gosat}
+                        onClick={() => setGosat(!gosat)}
+                        type="checkbox"
+                    />
+                    <label htmlFor={"gosat"}>GOSAT</label>
+                </div>
+                <div className={`${classes.container_style} ${darkClass}`}>
+                    <input
+                        id={"gosat_2"}
+                        className={`${classes.checkbox_style}`}
+                        checked={gosat2}
+                        onClick={() => setGosat2(!gosat2)}
+                        type="checkbox"
+                    />
+                    <label htmlFor={"gosat_2"}>GOSAT-2</label>
+                </div>
+                <div className={`${classes.container_style} ${darkClass}`}>
+                    <input
+                        id={"oco_2"}
+                        className={`${classes.checkbox_style}`}
+                        checked={oco2}
+                        onClick={() => setOco2(!oco2)}
+                        type="checkbox"
+                    />
+                    <label htmlFor={"oco_2"}>OCO-2</label>
+                </div>
+                <div className={`${classes.container_style} ${darkClass}`}>
+                    <input
+                        id={"oco_3"}
+                        className={`${classes.checkbox_style}`}
+                        checked={oco3}
+                        onClick={() => setOco3(!oco3)}
+                        type="checkbox"
+                    />
+                    <label htmlFor={"oco_3"}>OCO-3</label>
+                </div>
             </div>
             <div className={`${classes.flex_elem} ${darkClass}`}>
                 <SimpleParagraph children={t("selectGases")} darkmode={props.darkmode} />
-                <CustomRadio darkmode={props.darkmode} checked={true} id='co_2' name='gas' children='CO-2' />
-                <CustomRadio darkmode={props.darkmode} id='ch_4' name='gas' children='CH-4' />
+                <div className={`${classes.container_style} ${darkClass}`}>
+                    <input
+                        id={"co_2"}
+                        name={"gas"}
+                        className={classes.radio_style}
+                        checked={co2}
+                        onClick={changeGas}
+                        type="radio"
+                    />
+                    <label htmlFor={"co_2"}>CO-2</label>
+                </div>
+                <div className={`${classes.container_style} ${darkClass}`}>
+                    <input
+                        id={"ch_4"}
+                        name={"gas"}
+                        className={classes.radio_style}
+                        checked={ch4}
+                        onClick={changeGas}
+                        type="radio"
+                    />
+                    <label htmlFor={"ch_4"}>CH-4</label>
+                </div>
             </div>
             <div className={`${classes.flex_elem} ${darkClass}`} >
                 <SimpleParagraph children={t("displayQuality")} darkmode={props.darkmode} />
-                <CustomRadio darkmode={props.darkmode} checked={true} id='high' name='qual' children={t("high")} size='full' />
-                <CustomRadio darkmode={props.darkmode} id='middle' name='qual' children={t("middle")} size='full' />
-                <CustomRadio darkmode={props.darkmode} id='real' name='qual' children={t("realData")} size='full' />
+
+                <div className={`${classes.container_style} ${darkClass} ${classes.full}`}>
+                    <input
+                        id={"high"}
+                        name={"qual"}
+                        className={classes.radio_style}
+                        checked={high}
+                        onClick={() => {
+                            setHigh(true);
+                            setMiddle(false);
+                            setRealData(false);
+                        }}
+                        type="radio"
+                    />
+                    <label htmlFor={"high"}>{t("high")}</label>
+                </div>
+
+                <div className={`${classes.container_style} ${darkClass} ${classes.full}`}>
+                    <input
+                        id={"middle"}
+                        name={"qual"}
+                        className={classes.radio_style}
+                        checked={middle}
+                        onClick={() => {
+                            setHigh(false);
+                            setMiddle(true);
+                            setRealData(false);
+                        }}
+                        type="radio"
+                    />
+                    <label htmlFor={"middle"}>{t("middle")}</label>
+                </div>
+
+                <div className={`${classes.container_style} ${darkClass} ${classes.full}`}>
+                    <input
+                        id={"realData"}
+                        name={"qual"}
+                        className={classes.radio_style}
+                        checked={realData}
+                        onClick={() => {
+                            setHigh(false);
+                            setMiddle(false);
+                            setRealData(true);
+                        }}
+                        type="radio"
+                    />
+                    <label htmlFor={"realData"}>{t("realData")}</label>
+                </div>
             </div>
         </div>
     );
